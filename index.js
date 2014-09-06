a40779ddff872de92d3cb6fbfb2e4bf1232521b2
José Moreira <jmoreira@microdual.com>
2014-09-06 02:23:41 +0100

    minor changes

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+
 /**
-  The entry point.
-  @module Promise
-**/
-module.exports = require("./lib/promise")
+ * To use it, you just need to import it:
+ *
+ * ```js
+ * var Promise = require('findhit-promise');
+ * ```
+ *
+ * Based on [PromiseA+](http://promisesaplus.com/) Standartization, but slightly modified.
+ *
+ * @class Promise
+ */
+
+var Promise = require('./lib/class');
+
+// Extend it plus
+require('./lib/handler')( Promise );
+require('./lib/static.methods')( Promise );
+require('./lib/promiseify')( Promise );
+require('./lib/common')( Promise );
+require('./lib/util')( Promise );
+
+return Promise;
+
+// Export it
+module.exports = Promise;
+
+// TODO: AMDify
